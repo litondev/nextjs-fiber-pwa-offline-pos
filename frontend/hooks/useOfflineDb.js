e4be82a6728ef825ed226db38c5f738190ebf9a7
litoninot12345@gmail.com <litoninot12345@gmail.com>
2021-12-31 21:41:27 +0700

    compelete crud indexdb

--- frontend/hooks/useOfflineDb.js
+++ frontend/hooks/useOfflineDb.js
@@ -15,34 +15,33 @@ const useOfflineDb = (version = 1) => {
     setDb(database)
   }
 
-  const onUpgradeNeeded = (event) => {    
-    let database = event.target.result;
+  // const onUpgradeNeeded = (event) => {    
+  //   let database = event.target.result;
 
-    database.onerror = (event) => onError(event)
+  //   database.onerror = (event) => onError(event)
 
-    console.log("Fungsi ini akan terpangil jika ada perubahan versi db");
+  //   console.log("Fungsi ini akan terpangil jika ada perubahan versi db");
 
-    try{
-        let objectStoreCustomers = database.createObjectStore("categories", { keyPath: "ssn" , autoIncrement : true });
-        objectStoreCustomers.createIndex("id", "id", { unique: false });
-        objectStoreCustomers.createIndex("name", "name", { unique: false });
-        objectStoreCustomers.createIndex("description", "description", { unique: false });
-    }catch(err){
-        console.log(err)
-    }    
-  }
+  //   try{
+  //       let objectStoreCustomers = database.createObjectStore("categories", { keyPath: "ssn" , autoIncrement : true });
+  //       objectStoreCustomers.createIndex("id", "id", { unique: false });
+  //       objectStoreCustomers.createIndex("name", "name", { unique: false });
+  //       objectStoreCustomers.createIndex("description", "description", { unique: false });
+  //   }catch(err){
+  //       console.log(err)
+  //   }    
+  // }
 
   useEffect(() => {    
     if (!window.indexedDB) {
         console.log("Your browser doesn't support a stable version of IndexedDB. Such and such feature will not be available.");
     }else{
-        let requestDb = window.indexedDB.open("TestPos",version)
+        let requestDb = window.indexedDB.open("pos",4)
         requestDb.onerror = (event) => onError(event)
-        requestDb.onupgradeneeded = (event) => onUpgradeNeeded(event)
         requestDb.onsuccess = (event) => onSuccess(event)
         
         return () => {
-            requestDb.result.close();
+          // requestDb.result.close();
         }
     }
   }, [])
--- frontend/pages/pos/category/index.js
+++ frontend/pages/pos/category/index.js
@@ -4,6 +4,7 @@ import axiosServer from "@/librarys/axiosServer";
 import axiosClient  from "@/librarys/axiosClient";
 import { useRouter } from 'next/router'
 import {ToastError,ToastSuccess} from "@/librarys/toaster"
+import useOfflineDb from "@/hooks/useOfflineDb"
 import { useIndexedDBStore } from "use-indexeddb";
 import { Formik,Form,Field, ErrorMessage,useFormik } from 'formik';
 import * as Yup from 'yup';
@@ -16,6 +17,7 @@ const SigninSchema = Yup.object()
     
 export async function getServerSideProps(context) {
   let isSuccess = true 
+  let isOffline = false;
 
   let params = {
     search : context.query.search || '',
@@ -24,7 +26,7 @@ export async function getServerSideProps(context) {
   }
   
   try{
-    var {data}  = await axiosServer(context).get("/categorys",{params});        
+    var {data}  = await axiosServer(context).get("/category",{params});        
   }catch(err){     
     isSuccess = false    
 
@@ -34,8 +36,9 @@ export async function getServerSideProps(context) {
     }
 
     if(err.isAxiosError && !err.response){
-      var data = { data : [] ,isOffline : true}
+      var data = { data : []}
       isSuccess = true;
+      isOffline = true;
     }
   }
   
@@ -43,7 +46,8 @@ export async function getServerSideProps(context) {
     props: {
       isSuccess,
       data,
-      params
+      params,
+      isOffline
     }, 
   }
 }
@@ -53,14 +57,15 @@ export default function Category(props) {
   const router = useRouter();
   const [categories,setCategories] = useState({...props.data})
   const [params,setParams] = useState({...props.params})
+  const [isOffline,setMode] = useState(props.isOffline ? true : false)
   const [loadings,setLoadings] = useState({
     isDelete : false,
     isForm : false,
     isPage : false,
   })
 
-  const { add } = useIndexedDBStore("customers");  
-
+  const { add,openCursor,deleteAll ,update,deleteByID} = useIndexedDBStore("customers");  
+  const [db] = useOfflineDb(1)
   const { values, errors, handleChange, setFieldValue,setValues } = useFormik({
     initialValues: {
       id : 0,
@@ -71,14 +76,99 @@ export default function Category(props) {
   });
 
   useEffect(() => {
-    console.log("Hello")   
-  },[])
+    if(isOffline && db){
+      onLoadOffline()
+    }
+  },[db])
 
   useEffect(() => {
     setCategories({
       ...props.data
     })
   },[props.data])
+
+  const onLoadOffline = (limit = 5,page = 1) => {        
+    // onResetForm()
+    let result = []
+    let count = 0
+    let getSerach = params.search ? 0 : limit;
+
+    if(db){
+      console.log(db);
+      const transaction = db.transaction(['customers'], 'readonly');
+      const objectStore = transaction.objectStore('customers');
+
+      var countRequest = objectStore.count();
+      countRequest.onsuccess = function() {
+        let total_page = Math.ceil(countRequest.result / limit)
+        let start_page = (page - 1) * limit;
+        console.log("Page : "+page)    
+        console.log("Start Page : "+start_page)
+        console.log("Total Page : "+total_page)  
+        console.log("Limit : "+(limit+start_page))
+
+        objectStore.openCursor(null,'prev').onsuccess = function(event) {          
+          const cursor = event.target.result;
+            
+            if(cursor && (count <= (limit+start_page) || getSerach < limit)) {                                            
+              if(count >= start_page || getSerach < limit){
+                if(cursor.value.name.indexOf(params.search) >= 0){                  
+                  if(count >= start_page){
+                    result.push(cursor.value)      
+                    getSerach++;              
+                  }
+                }
+              }
+
+              cursor.continue();
+              count++
+            } else {                        
+
+              setCategories({
+                data : result,
+                page : page,
+                total_page : total_page,
+                per_page  : limit
+              })
+              console.log('Entries displayed backwards.');
+            }
+        }
+      }
+    }
+  }
+
+
+  const onNextOffline = () => {
+    let page = categories.page + 1;
+
+    if(page > categories.total_page){
+      page = categories.total_page
+    }
+
+    onLoadOffline(5,page)
+  }
+
+  const onBeforeOffline = () => {
+    let page = categories.page - 1;
+    if(page < 1){
+      page = 1;
+    }
+
+    onLoadOffline(5,page)
+  }
+
+  // 
+  const onSearchOffline = (event) => {
+    console.log(event.target.value)
+    setParams({
+      ...params,
+      search : event.target.value
+    })
+
+    if(event.key == 'Enter'){      
+      onLoadOffline(5,1)      
+    }
+  }  
 
   const onPage = (isNext) => {  
     router.push({
@@ -107,6 +197,18 @@ export default function Category(props) {
       });          
     }
   }
+  const onDeleteOffline = async (item) => {
+    try{
+      await deleteByID(item.ssn)
+     
+      ToastSuccess("Berhasil menghapus data")
+
+      onLoadOffline()
+    }catch(err){
+      console.log(err)
+      ToastError(err)
+    }      
+  }
 
   const onDelete = async (item) => {
     if(loadings.isDelete) return;
@@ -118,12 +220,19 @@ export default function Category(props) {
       })
       await axiosClient.delete("/category/"+item.id)
       ToastSuccess("Berhasil menghapus data")
+      setMode(false)
       router.push({
         pathname: router.pathname,
         query: props.params
       });        
     }catch(err){
-      ToastError(err)                        
+      if(err.isAxiosError && !err.response){
+        setMode(true)
+        onDeleteOffline(item)        
+      }else{
+        console.log(false)
+        ToastError(err)   
+      }                     
     }finally{
       setLoadings({
         ...loadings,
@@ -131,7 +240,6 @@ export default function Category(props) {
       })
     }
   }
-
    
   const onResetForm = () => {
     setValues({
@@ -142,19 +250,32 @@ export default function Category(props) {
     })    
   }
 
-  const onSubmitOfflie = async(values) => {
+  const onSubmitOffline = async(values) => {
     try{
+      if(!values.isEditable){
+        await add({ 
+          name:  values.name,
+          description : values.description,                            
+        })
+      }else{
+        await update({
+          ssn : values.ssn,
+          name : values.name,
+          description : values.description
+        })
+      } 
+     
       console.log(values)
-      await add({ 
-        name:  values.name,
-        description : values.description,                            
-      })
 
       ToastSuccess(`Berhasil ${values.isEditable ? 'Edit' : 'Tambah'} Data`)
+
+      onLoadOffline()
     }catch(err){
       console.log(err)
+      ToastError(err)
     }      
   }
+
   const onSubmit = async (values,{setErrors,setSubmitting}) => {      
     try{
       if(values.isEditable){
@@ -163,27 +284,28 @@ export default function Category(props) {
         await axiosClient.post("/category",values);
       }
 
-      onResetForm()
-
       ToastSuccess(`Berhasil ${values.isEditable ? 'Edit' : 'Tambah'} Data`)
+
+      setMode(false)
+
+      onResetForm()
 
       router.push({
         pathname: router.pathname,
         query: params
       });        
-
-    }catch(err){      
+    }catch(err){            
       if(err.isAxiosError && !err.response){
-        onSubmitOfflie(values)
-        return
+        setMode(true)
+        onSubmitOffline(values)        
+      }else{
+        console.log(err)
+        ToastError(err,setErrors)                        
       }
-      console.log(err)
-      ToastError(err,setErrors)                        
     }finally{
       setSubmitting(false)
     }  
   }
-
    
   const onEdit = (item) => {
     console.log(item)
@@ -243,17 +365,66 @@ export default function Category(props) {
         <br/>
       </div>
 
+      {!isOffline ? <div>
+        <div>
+          Searching 
+          <input type="text" onKeyUp={onSearch}/>
+        </div>
+
+        <table>
+          <thead>
+          <tr>
+            <td>Nama</td>
+            <td>Deskripsi</td>
+            <td>Opsi</td>
+          </tr>
+          </thead>
+          <tbody>
+          {categories.data.map(item => (
+            <tr key={item.id}>
+              <td>{item.name}</td>
+              <td>{item.description}</td>
+              <td>
+                <button onClick={() => onEdit(item)}>Edit</button>
+                <button onClick={() => onDelete(item)}>
+                  {loadings.isDelete ? '...' : 'Delete'}
+                </button>
+              </td>
+            </tr>
+          ))}
+          {!categories.data.length && (
+            <tr>
+              <td colSpan="3">
+                Data tidak ditemukan
+              </td>
+            </tr>
+          )}
+          </tbody>
+        </table>
+
+        {categories.page > 1 && <button onClick={() => onPage(0)}>
+          Sebelumnya
+        </button> }
+
+        {categories.total_page > categories.page && <button onClick={() => onPage(1)}>
+          Selanjutnya
+        </button>}
+      </div>
+      : <div>
       <div>
         Searching 
-        <input type="text" onKeyUp={onSearch}/>
+        <input type="text" onKeyUp={onSearchOffline}/>
       </div>
 
       <table>
+        <thead>
         <tr>
           <td>Nama</td>
           <td>Deskripsi</td>
           <td>Opsi</td>
         </tr>
+        </thead>
+        <tbody>
         {categories.data.map(item => (
           <tr key={item.id}>
             <td>{item.name}</td>
@@ -273,15 +444,17 @@ export default function Category(props) {
             </td>
           </tr>
         )}
+        </tbody>
       </table>
 
-      {categories.page > 1 && <button onClick={() => onPage(0)}>
-        Sebelumnya
-      </button> }
+      {categories.page > 1 && <button onClick={() => onBeforeOffline()}>
+          Sebelumnya
+        </button> }
 
-      {categories.total_page > categories.page && <button onClick={() => onPage(1)}>
-        Selanjutnya
-      </button>}
+        {categories.total_page > categories.page && <button onClick={() => onNextOffline()}>
+          Selanjutnya
+        </button>}
+    </div>}
     </DefaultLayout>
   )
 }
